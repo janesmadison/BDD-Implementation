b340134c38f00b8bf1899783636b9e51e419151d
Madison Janes <madisonoliviajanes@gmail.com>
2020-07-09 15:26:56 -0400

    Test case

--- src/test/java/bdd/BDDTest.java
+++ src/test/java/bdd/BDDTest.java
@@ -26,12 +26,15 @@ public class BDDTest {
     public void testVBDD() {
 
     VBDDManager tree = new VBDDManager();
-    VBDD<Integer> x =  tree.choice("x", 1, 0);
-  //  VBDD<Boolean> y =  tree.choice("y", false, true);
+  //  VBDD<Integer> x =  tree.choice("x", 1, 0);
+    VBDD<Boolean> a =  tree.choice("a", false, true);
+    VBDD<Boolean> b =  tree.choice("b", false, true);
+    VBDD<Boolean> c =  tree.choice("c", false, true);
+    tree.printDot(tree.ite_(a, b, c));
     
   //  assertEquals(x.flatMap(a -> y.map(b -> a + b)), tree.choice("x", tree.choice("y", 3, 1), tree.choice("y", 2, 0)));   
    // assertEquals(x.flatMap(a -> tree.one(a)));
-    tree.printDot( x.<Integer>flatMap(a -> tree.choice("z", a , a + 1)));
+//    tree.printDot( x.<Integer>flatMap(a -> tree.choice("z", a , a + 1)));
 //    tree.printDot(tree.choice("x", tree.choice("y", 3, 1), tree.choice("y", 2, 0)));
     // tree.printDot((VBDD<Integer>) x.map(a -> a + 2));
 //     tree.printDot((VBDD<Boolean>) y.map(a -> a && true));
